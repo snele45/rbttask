b982b05abbb17cd15df390869795583aca593b65
snele45 <sneskovic4@hotmail.com>
2024-07-10 13:22:48 +0200

    Transform task

--- dags/rbt_dag.py
+++ dags/rbt_dag.py
@@ -11,12 +11,25 @@ def read_csvs(**kwargs):
     df1_json = df1.to_json()
     df2_json = df2.to_json()
     
-
+    
     kwargs['ti'].xcom_push(key='zoo_animals', value=df1_json)
     kwargs['ti'].xcom_push(key='zoo_health_records', value=df2_json)
 
 
+def transform_df(**kwargs):
+    df1_json = kwargs['ti'].xcom_pull(key='zoo_animals', task_ids='extract_task')
+    df2_json = kwargs['ti'].xcom_pull(key='zoo_health_records', task_ids='extract_task')
 
+    df1 = pd.read_json(df1_json)
+    df2 = pd.read_json(df2_json)
+
+    df = pd.merge(df1,df2,on='animal_id')
+    df = df[df['age'] >=2]
+    
+    df['animal_name'] = df['animal_name'].str.title()
+
+    df = df[df['health_status'].isin(['Healthy', 'Needs Attention'])]
+    
 
 
 dag = DAG(
@@ -28,12 +41,19 @@ dag = DAG(
 
 
 extract_task = PythonOperator(
-    task_id='read_csvs',
+    task_id='extract_task',
     python_callable=read_csvs,
     provide_context=True,
     dag=dag,
 )
 
+transform_task = PythonOperator(
+    task_id = 'transform_task',
+    python_callable=transform_df,
+    provide_context = True,
+    dag = dag,
+)
 
 
-extract_task
+
+extract_task >> transform_task
